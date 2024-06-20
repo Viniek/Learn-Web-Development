8ba1763867c85df0ca58fbbced76850c45c73e45
unknown <njerivictory52@gmail.com>
2024-06-20 23:00:02 +0300

    use of functions in express

--- zustad/src/CounterUp.jsx
+++ zustad/src/CounterUp.jsx
@@ -3,6 +3,7 @@ import userCounterStore from './store/counterStore'
 
 function CounterUp() {
     const count =userCounterStore((state) => state.count)
+    const addCount= userCounterStore(state =>state.addCount)
   return (
    <>
    <button>+ count is {count} </button>
--- zustad/src/store/counterStore.js
+++ zustad/src/store/counterStore.js
@@ -2,6 +2,12 @@
 import {create} from 'zustand'
 const counterStore= (set) =>({
     count:25
+
+    addCount : ()=> {
+        set((previousState)=>{
+            console.log(previousState);
+    })
+    }
 })
 const userCounterStore =create (counterStore);
 

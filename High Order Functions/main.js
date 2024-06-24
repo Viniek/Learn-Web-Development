91d0cc39725c91461976d4db40e98bca067b6d61
unknown <njerivictory52@gmail.com>
2024-06-24 20:32:43 +0300

    map

--- High Order Functions/main.js
+++ High Order Functions/main.js
@@ -1,9 +1,15 @@
 const names=["Vee","Njee","Victory","Njerii","Njerihh"];
- 
+//  for each
 names.forEach((currentElement)=>{
-const namesToLowerCase=currentElement.toLowerCase();
+const namesToLowerCase='Pretty lady'+" "+ currentElement.toLowerCase();
 console.log(namesToLowerCase)
-//   const namesToLowerCase ='Pretty Lady: ${currentElement.toLowerCase()}'
-//   console.log(namesToLowerCase);
 })
 console.log(names)
+// map
+const emails=names.map((currentElement)=>{
+    let emailsToLowerCase=currentElement.toLowerCase();
+    return emailsToLowerCase+ "@teach2give"
+})
+console.log(emails)
+// filter
+const namesThatStart

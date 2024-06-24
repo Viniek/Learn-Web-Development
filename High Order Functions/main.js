9b5056a1809f65926b86cf823b56eb63d845ac12
unknown <njerivictory52@gmail.com>
2024-06-24 20:03:26 +0300

    for each

--- High Order Functions/main.js
+++ High Order Functions/main.js
@@ -1,3 +1,9 @@
-const names=["victory","njerii","njee","vee","vickie"];
- names.forEach((currentElement)=>
- console.log(currentElement));
+const names=["Vee","Njee","Victory","Njerii","Njerihh"];
+ 
+names.forEach((currentElement)=>{
+const namesToLowerCase=currentElement.toLowerCase();
+console.log(namesToLowerCase)
+//   const namesToLowerCase ='Pretty Lady: ${currentElement.toLowerCase()}'
+//   console.log(namesToLowerCase);
+})
+console.log(names)
